235ce5121948c8b6c01c3f77410161587b88d305
Jayakrishnan <jayakrishnan1012@gmail.com>
2022-05-24 15:29:48 +0530

    Add react loading progress component using nProgress

---
+++ React/loadingProgress/app.jsx
@@ -0,0 +1,11 @@
+import "@styles/nprogress.css";
+import NProgress from "../components/nprogress";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <NProgress />
+    </>
+  );
+}
